1563e4aaa5eb046b444910c07f791026372b1baa
Rdadaiya Krinish <47221638+krinish291@users.noreply.github.com>
2020-06-30 00:45:30 +0530

    counter

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const countEl = document.getElementById('count');
 updateVisitCount();
 
 function updateVisitCount() {
-	fetch('https://api.countapi.xyz/update/florin-popcom/codepen/?amount=1')
+	fetch('https://api.countapi.xyz/update/krinish291.github.io/?amount=1')
 	.then(res => res.json())
 	.then(res => {
 		countEl.innerHTML = res.value;
