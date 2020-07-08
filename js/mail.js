5f65fafec81067fa9cc97f36d19c79f2d59e5765
Rdadaiya Krinish <47221638+krinish291@users.noreply.github.com>
2020-07-08 23:41:27 +0530

    sending mail

--- js/mail.js
+++ js/mail.js
@@ -2,15 +2,38 @@
 
 function sendmail(){
     console.log("sendmail call");
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "radadiyakrinish1999@gmail.com",
-        Password : "3D92F2858C197BC99873185F372F1C4B1217",
-        To : 'radadiyakrinish1999@gmail.com',
-        From : "radadiyakrinish29@gmail.com",
-        Subject : "check mail",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
+    let name=document.getElementById("name").value;
+    let subject=document.getElementById("subject").value;
+    let email=document.getElementById("email").value;
+    let msg=document.getElementById("msg").value;
+    console.log(name);
+    console.log(subject);
+    console.log(email);
+    console.log(msg);
+    
+    if(!name || !msg || !subject||!email){
+      alert("Enter all valid Details.");
+    }
+    else{     
+         Email.send({
+           Host : "smtp.elasticemail.com",
+           Username : "radadiyakrinish29@gmail.com",
+           Password : "2FD125D722486A49CB300EF5BD82D9D99067",
+           To : 'radadiyakrinish1999@gmail.com',
+           From :email,
+           Subject : subject,
+           Body : msg
+       }).then(
+         message => alert(message)
+        
+       );
+        document.getElementById("name").value="";
+        document.getElementById("subject").value="";
+        document.getElementById("email").value="";
+        document.getElementById("msg").value="";
+       console.log("success");
+       
+      
+    }
+   
 }
